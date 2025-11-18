180c75f853adec0434d6324752cab90292f3e0d6
zhaoShuai <s74933347@gmail.com>
2025-11-19 00:09:50 +0800

    update some config

---
+++ src/test/java/com/example/sdcs/SimpleDistributedCacheSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sdcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDistributedCacheSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
